35e745080cf6aba21f56ef4a11b7d3b0008203ea
Rami Loiferman <ramiloif@gmail.com>
2024-04-05 12:37:42 +0300

    bacground (#8)
    Co-authored-by: Roman Loiferman <romanloiferman@Romans-MBP.lan>

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,28 @@ import Home from "./components/home";
 import Welcome from "./components/sign-up";
 import { UserProvider } from "./user-context";
 import Playlist from "./components/playlist";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        html, body, #root {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          width: 100%;
+          height: 100%;
+        }
+      `,
+    },
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
     <BrowserRouter>
       <UserProvider>
         <Routes>
@@ -21,6 +40,7 @@ function App() {
         </Routes>
       </UserProvider>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
